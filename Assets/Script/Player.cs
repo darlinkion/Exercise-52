4294b20fbc0ba68280026dd38cd64b9c3a15db8f
darlinkion <49286517+darlinkion@users.noreply.github.com>
2022-12-17 22:24:44 +0300

    2 commit

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//[RequireComponent(typeof(HealthBar))]
-
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
@@ -12,33 +10,25 @@ public class Player : MonoBehaviour
 
     private int _currentHealth;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _healthBar.SetMaxHealth(_maxHealth);
         _currentHealth = _maxHealth;
-    }
-
-    // Update is called once per frame
-    private void FixedUpdate()
-    {
-        
     }
 
     public void TakeAttack(int damage)
     {
         _currentHealth -= damage;
         _healthBar.SetHealth(_currentHealth);
-
     }
 
     public void Heal(int health)
     {
-        if((_currentHealth+health) <=_maxHealth)
+        if ((_currentHealth + health) <= _maxHealth)
         {
             _currentHealth += health;
             _healthBar.SetHealth(_currentHealth);
-            
+
         }
     }
 }
