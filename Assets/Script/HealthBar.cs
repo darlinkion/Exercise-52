f4dcd63882c81aad6845dcd685c1141cf482029e
darlinkion <49286517+darlinkion@users.noreply.github.com>
2022-12-24 23:47:53 +0300

    HealthBar changed with UnityEvents

--- Assets/Script/HealthBar.cs
+++ Assets/Script/HealthBar.cs
@@ -2,30 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private Gradient _gradient;
     [SerializeField] private Image _fill;
+    [SerializeField] private Player _player;
 
     private WaitForFixedUpdate _waitForFixedUpdate;
     private float _deltaHealth;
-
-    public void SetMaxHealth(int health)
-    {
-        _slider.maxValue = health;
-        _slider.value = health;
-        _waitForFixedUpdate = new WaitForFixedUpdate();
-        _deltaHealth = 1f;
-
-        _gradient.Evaluate(_slider.normalizedValue);
-    }
-
+    
     public void SetHealth(int health)
     {
         StopCoroutine(MoveScale(health));
         StartCoroutine(MoveScale(health));
+    }
+
+    private void Start()
+    {
+        _slider.maxValue = _player.MaxHealth;
+        _slider.value = _player.MaxHealth;
+        _waitForFixedUpdate = new WaitForFixedUpdate();
+        _deltaHealth = 1f;
+
+        _gradient.Evaluate(_slider.normalizedValue);
     }
 
     private IEnumerator MoveScale(int health)
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
     [SerializeField] private HealthBar _healthBar;
+    [SerializeField] private UnityEvent<int> _setHealthBar;
 
     private int _currentHealth;
+    private int _minHealth;
+
+    public int MaxHealth => _maxHealth;
 
     private void Start()
     {
-        _healthBar.SetMaxHealth(_maxHealth);
+        _minHealth = 0;
         _currentHealth = _maxHealth;
     }
 
     public void TakeAttack(int damage)
     {
-        _currentHealth -= damage;
-        _healthBar.SetHealth(_currentHealth);
+        if (_currentHealth - damage <= _minHealth)
+        {
+            _currentHealth = _minHealth;
+        }
+        else
+        {
+            _currentHealth -= damage;
+        }
+
+        _setHealthBar.Invoke(_currentHealth);
     }
 
     public void Heal(int health)
@@ -27,8 +40,7 @@ public class Player : MonoBehaviour
         if ((_currentHealth + health) <= _maxHealth)
         {
             _currentHealth += health;
-            _healthBar.SetHealth(_currentHealth);
-
+            _setHealthBar.Invoke(_currentHealth);
         }
     }
 }
